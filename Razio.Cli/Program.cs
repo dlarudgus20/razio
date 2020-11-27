a70e6c3daa008aa8648c5f0029037aa3fdd55789
KyeongHyeon Im <kyeonghyeon.im@kongstudios.net>
2020-11-27 19:58:28 +0900

    initial commit

---
+++ Razio.Cli/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+const string map = @"########################################
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+O                                       O
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+########################################";
+
+(int x, int y) position = (19, 9);
+
+Console.Clear();
+
+while (true)
+{
+    var keyInfo = Console.ReadKey();
+
+    Console.SetCursorPosition(2, 2);
+    Console.Write(map);
+}
